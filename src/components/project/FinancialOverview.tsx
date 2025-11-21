3499ae8cef500322dd44b7caabf4a70fe12ebd83
Wes Gray <wes@floorsunlimitedusa.com>
2025-11-21 16:28:02 -0600

    Fix layout: Profit and Cash Flow headings on same row
    - Profit stays in left column
    - Cash Flow now in right column at same level as Profit
    - Both section headings align horizontally

--- src/components/project/FinancialOverview.tsx
+++ src/components/project/FinancialOverview.tsx
@@ -176,26 +176,6 @@ export function FinancialOverview({
               </Table>
             </>
           )}
-
-          {isDesktop && (
-            <>
-              <SectionHeading title="Cash Flow" marginTop />
-              <Table>
-                <Row label="Cash In" value={money(financials.cashIn)} bold />
-                <Row label="Cash Out" value={money(financials.cashOut)} bold />
-                <Row
-                  label="Net Cash Flow"
-                  value={money(financials.netCashFlow)}
-                  bold
-                />
-                <Row
-                  label="Cash Position (+/-)"
-                  value={pct(financials.cashPositionPercent)}
-                  bold
-                />
-              </Table>
-            </>
-          )}
         </div>
 
         {/* Cost column */}
@@ -257,10 +237,29 @@ export function FinancialOverview({
                   value={pct(financials.percentCompleteCost)}
                   bold
                 />
-                <Row label="" value="" />
               </>
             )}
           </Table>
+
+          {isDesktop && (
+            <>
+              <SectionHeading title="Cash Flow" marginTop />
+              <Table>
+                <Row label="Cash In" value={money(financials.cashIn)} bold />
+                <Row label="Cash Out" value={money(financials.cashOut)} bold />
+                <Row
+                  label="Net Cash Flow"
+                  value={money(financials.netCashFlow)}
+                  bold
+                />
+                <Row
+                  label="Cash Position (+/-)"
+                  value={pct(financials.cashPositionPercent)}
+                  bold
+                />
+              </Table>
+            </>
+          )}
 
           {!isDesktop && (
             <>
